fe9aa1fd4041c8c2d10340d853a7e3b301aae0c6
duanyufei <duanyufei@bytedance.com>
2025-12-10 02:19:00 +0800

    optimize: HomePage UI

--- app/src/main/java/felix/duan/superherodb/ui/widget/Widgets.kt
+++ app/src/main/java/felix/duan/superherodb/ui/widget/Widgets.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
@@ -29,7 +31,6 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ModifierLocalBeyondBoundsLayout
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -458,25 +459,38 @@ fun HomePage(
     onSearchClick: () -> Unit,
     modifier: Modifier = Modifier.Companion,
 ) {
-    Column(
+    Box(
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.Companion.CenterHorizontally
+            .padding(16.dp)
     ) {
-        Text("Super Hero DB")
-        Button(
-            onClick = onBrowseClick,
-            modifier = Modifier.fillMaxWidth()
+        Text(
+            text = "Super Hero DB",
+            style = MaterialTheme.typography.displayMedium,
+            modifier = Modifier.align(Alignment.Center)
+        )
+
+        Column(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
-            Text("Browse")
-        }
-        Button(
-            onClick = onSearchClick,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            Text("Search")
+            Button(
+                onClick = onBrowseClick,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text("Browse")
+            }
+            Button(
+                onClick = onSearchClick,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 20.dp)
+            ) {
+                Text("Search")
+            }
         }
     }
 }
