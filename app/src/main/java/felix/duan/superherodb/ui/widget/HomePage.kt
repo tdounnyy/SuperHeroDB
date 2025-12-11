f74e789b88ff4c6ec4678d7743fd689081ba8b29
duanyufei <duanyufei@bytedance.com>
2025-12-11 10:11:58 +0800

    feat: impl Button to clear local database

--- app/src/main/java/felix/duan/superherodb/ui/widget/HomePage.kt
+++ app/src/main/java/felix/duan/superherodb/ui/widget/HomePage.kt
@@ -7,12 +7,18 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import felix.duan.superherodb.repo.LocalRepo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @Composable
 fun HomePage(
@@ -34,6 +40,7 @@ fun HomePage(
         Column(
             modifier = Modifier.Companion
                 .align(Alignment.Companion.BottomCenter)
+                .padding(bottom = 20.dp)
                 .fillMaxWidth(),
             horizontalAlignment = Alignment.Companion.CenterHorizontally,
             verticalArrangement = Arrangement.spacedBy(16.dp)
@@ -46,11 +53,22 @@ fun HomePage(
             }
             Button(
                 onClick = onSearchClick,
-                modifier = Modifier.Companion
-                    .fillMaxWidth()
-                    .padding(bottom = 20.dp)
+                modifier = Modifier.Companion.fillMaxWidth()
             ) {
                 Text("Search")
+            }
+            Button(
+                onClick = {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        LocalRepo.clearAll()
+                    }
+                },
+                modifier = Modifier.Companion.fillMaxWidth(),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFF8B0000)
+                )
+            ) {
+                Text("Clear Local DB")
             }
         }
     }
